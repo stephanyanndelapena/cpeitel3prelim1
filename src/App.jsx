441ad385ae688b6dda952b925e310469124ef4ef
stephanyanndelapena <delapenastephanyann@gmail.com>
2026-02-24 14:02:36 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>STEPHANY ANN DELA PENA</h1> 
+      <div className="layout">
+        <div className="item item1">1</div>
+        <div className="item item2">2</div>
+        <div className="item item3">3</div>
+        <div className="item item4">4</div>
+        <div className="item item5">5</div>
+        <div className="item item6">6</div>
+        <div className="item item7">7</div>
+        <div className="item item8">8</div>
+      </div>
+    </>
+  )
+}
+
+export default App
